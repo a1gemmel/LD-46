19ab8c6633d60d7b367c3add37b9f8b2f81f00bd
a1gemmel <andrew.gemmel@live.ca>
2020-04-17 18:42:43 -0600

    Add game window

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import PIXI from "pixi.js"
+import * as PIXI from "pixi.js"
+
+
+//Create a Pixi Application
+let app = new PIXI.Application({width: 512, height: 512});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.getElementById("game").appendChild(app.view);
